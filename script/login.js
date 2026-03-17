a1e35275ed7c20a21800a8e60d2f1ef3ebec3692
Sabbir Hossain Mondol <sabbirhossain25112000@gmail.com>
2026-03-18 01:59:45 +0600

    first logging

---
+++ script/login.js
@@ -0,0 +1,20 @@
+console.log("Login script loaded");
+ document.getElementById("loginBtn").addEventListener("click", function(event) {
+    console.log("Login button clicked");
+    const MobileNumber= document.getElementById("input-number");
+    const Number= MobileNumber.value;
+    console.log("Mobile number entered:", Number);
+    const passwordInput = document.getElementById("input-pin");
+    const password = passwordInput.value;
+    console.log("Password entered:", password);
+    if (Number === "1234567890" && password === "1234") {
+        alert("Login successful");
+        window.location.assign("home.html");
+    }
+        else {
+        
+        alert("Login failed");
+        return  ;
+    }
+
+ });
